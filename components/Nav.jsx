baeb968b7435bfd52ca1dba033f208d27614c66b
DennisJustesen16 <1086221@ucn.dk>
2022-08-23 14:45:28 +0200

    chapter 2

---
+++ components/Nav.jsx
@@ -0,0 +1,14 @@
+import styles from '../styles/Nav.module.css';
+
+export default function Nav(){
+    return(
+        <nav className={styles.nav}>
+            <h1>Emerald DApp</h1>
+            <button>Log In</button>
+        </nav>
+
+
+
+
+    )
+}
